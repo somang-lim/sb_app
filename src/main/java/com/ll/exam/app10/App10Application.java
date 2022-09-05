1b8000ef654cc174960cbef2a74c707c1b1e265b
jhs512 <jangka512@gmail.com>
2022-09-05 14:42:34 +0900

    추가 세팅

--- src/main/java/com/ll/exam/app10/App10Application.java
+++ src/main/java/com/ll/exam/app10/App10Application.java
@@ -2,12 +2,14 @@ package com.ll.exam.app10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class App10Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App10Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App10Application.class, args);
+    }
 
 }
