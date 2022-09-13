9369de3c9927d8b9a2899ad2e1a47ca5131515c2
jhs512 <jangka512@gmail.com>
2022-09-13 13:42:34 +0900

    테스트 케이스 구현

--- src/test/java/com/ll/exam/app10/AppTests.java
+++ src/test/java/com/ll/exam/app10/AppTests.java
@@ -1,13 +1,13 @@
 package com.ll.exam.app10;
 
 import com.ll.exam.app10.app.home.controller.HomeController;
+import com.ll.exam.app10.app.member.controller.MemberController;
 import com.ll.exam.app10.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,6 +16,7 @@ import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -59,15 +60,43 @@ class AppTests {
 
     @Test
     @DisplayName("user1로 로그인 후 프로필페이지에 접속하면 user1의 이메일이 보여야 한다.")
-    @Rollback(false)
     void t3() throws Exception {
-        // mockMvc로 로그인 처리
+        // WHEN
+        // GET /
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/member/profile")
+                                .with(user("user1").password("1234").roles("user"))
+                )
+                .andDo(print());
+
+        // THEN
+        // 안녕
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showProfile"))
+                .andExpect(content().string(containsString("user1@test.com")));
     }
 
     @Test
     @DisplayName("user4로 로그인 후 프로필페이지에 접속하면 user4의 이메일이 보여야 한다.")
-    @Rollback(false)
     void t4() throws Exception {
-        // mockMvc로 로그인 처리
+        // WHEN
+        // GET /
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/member/profile")
+                                .with(user("user4").password("1234").roles("user"))
+                )
+                .andDo(print());
+
+        // THEN
+        // 안녕
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showProfile"))
+                .andExpect(content().string(containsString("user4@test.com")));
     }
 }
