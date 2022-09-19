2b9c5bd64f3140c658b2a15a3eef9d0e0015e2ad
somang-lim <somangjuha@gmail.com>
2022-09-19 15:31:08 +0900

    상세 페이지

--- src/main/java/com/ll/exam/app10/app/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/app10/app/article/service/ArticleService.java
@@ -24,4 +24,8 @@ public class ArticleService{
 
                 return article;
         }
+
+    public Article getArticleById(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
 }
