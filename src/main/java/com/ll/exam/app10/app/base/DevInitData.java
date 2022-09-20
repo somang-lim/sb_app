544e8fd58e07cecf0ecdb3fec4627a1866dd5fa2
somang-lim <somangjuha@gmail.com>
2022-09-20 16:11:55 +0900

    각 게시물별로 이미지가 존재하는 만큼 노출

--- src/main/java/com/ll/exam/app10/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/app10/app/base/DevInitData.java
@@ -24,9 +24,15 @@ public class DevInitData {
             Member member2 = memberService.join("user2", password, "user2@test.com");
             memberService.setProfileImgByUrl(member2, "https://picsum.photos/200/300");
 
-            Article article = articleService.write(member1, "제목", "내용");
-            articleService.addGenFileByUrl(article, "common", "inBody", 1, "https://picsum.photos/200/300");
-            articleService.addGenFileByUrl(article, "common", "inBody", 2, "https://picsum.photos/200/300");
+            Article article1 = articleService.write(member1, "제목1", "내용1");
+            articleService.addGenFileByUrl(article1, "common", "inBody", 1, "https://picsum.photos/200/300");
+            articleService.addGenFileByUrl(article1, "common", "inBody", 2, "https://picsum.photos/200/300");
+            articleService.addGenFileByUrl(article1, "common", "inBody", 3, "https://picsum.photos/200/300");
+            articleService.addGenFileByUrl(article1, "common", "inBody", 4, "https://picsum.photos/200/300");
+
+            Article article2 = articleService.write(member1, "제목2", "내용2");
+            articleService.addGenFileByUrl(article2, "common", "inBody", 1, "https://picsum.photos/200/300");
+            articleService.addGenFileByUrl(article2, "common", "inBody", 2, "https://picsum.photos/200/300");
         };
     }
 }
